34ade383b6fc1d2fc830cf5fd6c43e3d1c3b933c
Banee Ishaque K <baneeishaque@gmail.com>
2017-03-20 21:27:39 +0530

    bill registration second fix

--- android/register_bill.php
+++ android/register_bill.php
@@ -4,6 +4,7 @@ include_once 'config.php';
 $item_amount_string = filter_input(INPUT_POST, 'item_amount');
 //echo $item_amount;
 $items = explode("-", $item_amount_string);
+//print_r($items);
 $total_amount = filter_input(INPUT_POST, 'total_amount');
 $shop = filter_input(INPUT_POST, 'shop');
 $sql = "INSERT INTO `account`(`day`, `month`, `year`, `total-amount`, `shop-id`) VALUES (DATE_FORMAT(NOW(),'%d'),DATE_FORMAT(NOW(),'%m'),DATE_FORMAT(NOW(),'%Y'),$total_amount,$shop)";
@@ -13,18 +14,22 @@ if ($con->query($sql)) {
     $result = $con->query($sql);
     $row = mysqli_fetch_assoc($result);
     $bill_no = $row['id'] + 1;
+
     foreach ($items as &$value) {
 //        echo $value;
         $values = explode(":", $value);
 //        echo $values[0];
 //        echo $values[1];
-        $sql = "INSERT INTO `bill`(`id`, `product-name`, `amount`) VALUES ($bill_no,'$values[0]',0)";
+        $sql = "INSERT INTO `bill`(`id`, `product-name`, `amount`) VALUES ($bill_no,'$values[0]','$values[1]')";
         $con->query($sql);
     }
 
     $sql2 = "INSERT INTO `purchase`(`bill-no`, `shop-id`,`amount`, `customer-name`, `customer-mob`, `day`, `month`, `year`) VALUES ($bill_no,$shop,$total_amount,'samad','9999999999',DATE_FORMAT(NOW(),'%d'),DATE_FORMAT(NOW(),'%m'),DATE_FORMAT(NOW(),'%Y'))";
-    $con->query($sql2);
-    echo "0";
+    if ($con->query($sql2)) {
+        echo "0";
+    } else {
+        echo mysqli_error($con);
+    }
 } else {
     echo mysqli_error($con);
 }
